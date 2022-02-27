4d03ae5d3108e09056d8fbc64a9fadea5b171065
okan kaipoglu <kaipoglu267@gmail.com>
2022-02-27 04:04:02 +0300

    school_ado eklendi

---
+++ school_ado/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace school_ado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnlist = new System.Windows.Forms.Button();
+            this.btnara = new System.Windows.Forms.Button();
+            this.btnsil = new System.Windows.Forms.Button();
+            this.btnguncelle = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.btnekle = new System.Windows.Forms.Button();
+            this.txtsoyad = new System.Windows.Forms.TextBox();
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.silToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Coral;
+            this.panel1.Controls.Add(this.btnlist);
+            this.panel1.Controls.Add(this.btnara);
+            this.panel1.Controls.Add(this.btnsil);
+            this.panel1.Controls.Add(this.btnguncelle);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtid);
+            this.panel1.Controls.Add(this.btnekle);
+            this.panel1.Controls.Add(this.txtsoyad);
+            this.panel1.Controls.Add(this.txtad);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(641, 132);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnlist
+            // 
+            this.btnlist.Location = new System.Drawing.Point(339, 95);
+            this.btnlist.Name = "btnlist";
+            this.btnlist.Size = new System.Drawing.Size(75, 23);
+            this.btnlist.TabIndex = 8;
+            this.btnlist.Text = "LİSTELE";
+            this.btnlist.UseVisualStyleBackColor = true;
+            this.btnlist.Click += new System.EventHandler(this.btnlist_Click);
+            // 
+            // btnara
+            // 
+            this.btnara.Location = new System.Drawing.Point(443, 95);
+            this.btnara.Name = "btnara";
+            this.btnara.Size = new System.Drawing.Size(75, 23);
+            this.btnara.TabIndex = 7;
+            this.btnara.Text = "ARA";
+            this.btnara.UseVisualStyleBackColor = true;
+            this.btnara.Click += new System.EventHandler(this.btnara_Click);
+            // 
+            // btnsil
+            // 
+            this.btnsil.Location = new System.Drawing.Point(542, 95);
+            this.btnsil.Name = "btnsil";
+            this.btnsil.Size = new System.Drawing.Size(75, 23);
+            this.btnsil.TabIndex = 6;
+            this.btnsil.Text = "SİL";
+            this.btnsil.UseVisualStyleBackColor = true;
+            // 
+            // btnguncelle
+            // 
+            this.btnguncelle.Location = new System.Drawing.Point(241, 95);
+            this.btnguncelle.Name = "btnguncelle";
+            this.btnguncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnguncelle.TabIndex = 5;
+            this.btnguncelle.Text = "GUNCELLE";
+            this.btnguncelle.UseVisualStyleBackColor = true;
+            this.btnguncelle.Click += new System.EventHandler(this.btnguncelle_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(26, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "SOYAD";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(65, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "AD";
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(283, 13);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(100, 20);
+            this.txtid.TabIndex = 3;
+            this.txtid.Visible = false;
+            // 
+            // btnekle
+            // 
+            this.btnekle.Location = new System.Drawing.Point(148, 95);
+            this.btnekle.Name = "btnekle";
+            this.btnekle.Size = new System.Drawing.Size(75, 23);
+            this.btnekle.TabIndex = 2;
+            this.btnekle.Text = "EKLE";
+            this.btnekle.UseVisualStyleBackColor = true;
+            this.btnekle.Click += new System.EventHandler(this.btnekle_Click);
+            // 
+            // txtsoyad
+            // 
+            this.txtsoyad.Location = new System.Drawing.Point(123, 59);
+            this.txtsoyad.Name = "txtsoyad";
+            this.txtsoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtsoyad.TabIndex = 1;
+            // 
+            // txtad
+            // 
+            this.txtad.Location = new System.Drawing.Point(123, 13);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(100, 20);
+            this.txtad.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(641, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.MenuHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 132);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(641, 296);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.silToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 48);
+            // 
+            // silToolStripMenuItem
+            // 
+            this.silToolStripMenuItem.Name = "silToolStripMenuItem";
+            this.silToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.silToolStripMenuItem.Text = "Sil";
+            this.silToolStripMenuItem.Click += new System.EventHandler(this.silToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnlist;
+        private System.Windows.Forms.Button btnara;
+        private System.Windows.Forms.Button btnsil;
+        private System.Windows.Forms.Button btnguncelle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.Button btnekle;
+        private System.Windows.Forms.TextBox txtsoyad;
+        private System.Windows.Forms.TextBox txtad;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem silToolStripMenuItem;
+    }
+}
+
---
+++ school_ado/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+
+
+namespace school_ado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+          string yol = "Data Source=DESKTOP-014OO3I;Initial Catalog=okanokul;Integrated Security=True";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string komutcumlesi = "select  * from personel";
+
+            using (SqlConnection baglanti = new SqlConnection() )
+            {
+                baglanti.ConnectionString = yol;
+
+                using (SqlCommand listelemekomutu = new SqlCommand(komutcumlesi, baglanti))
+                {
+                    baglanti.Open();
+
+                    using (DataTable datatable = new DataTable())
+                    {
+                        datatable.Columns.Add("personelKimlikNo");
+                        datatable.Columns.Add("Adı");
+                        datatable.Columns.Add("Soyadı");
+
+                        using (SqlDataReader rdr = listelemekomutu.ExecuteReader())
+                        {
+                            while (rdr.Read())
+                            {
+
+                                DataRow row = datatable.NewRow();
+                                row["personelKimlikNo"] = rdr["personelID"];
+                                row["Adı"] = rdr["adi"];
+                                row["Soyadı"] = rdr["soyadi"];
+                                datatable.Rows.Add(row);
+
+                            }
+                        }
+
+                        dataGridView1.DataSource = datatable;
+                    }
+
+                    baglanti.Close();
+
+
+                    if (baglanti.State == System.Data.ConnectionState.Closed)
+                    {
+                        toolStripStatusLabel1.Text = "kapalı";
+                    }
+                    else
+                    {
+                        toolStripStatusLabel1.Text = "açık";
+                    }
+
+                    dataGridView1.Columns[0].Visible = false;
+                    dataGridView1.Columns[1].Width = 400;
+                    dataGridView1.Columns[2].Width = 230;
+
+
+
+
+                }
+            }
+
+        }
+
+        private void btnara_Click(object sender, EventArgs e)
+        {
+
+            if (txtid.Text == "")
+            {
+                MessageBox.Show("lütfen aranacak veriyi giriniz");
+                return;
+            }
+
+            string sorgu = "select * from personel where personelID=" + txtid.Text;
+
+            using (SqlConnection baglanti = new SqlConnection())
+            {
+                baglanti.ConnectionString = yol;
+                using (SqlDataAdapter adapter = new SqlDataAdapter(sorgu, baglanti))
+                {
+                    using (DataSet ds = new DataSet())
+                    {
+                        adapter.Fill(ds, "personel");
+                        dataGridView1.DataSource = ds.Tables["personel"];
+                    }
+                }
+            }
+
+
+        }
+
+        private void btnekle_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection baglanti = new SqlConnection())
+            {
+                baglanti.ConnectionString = yol;
+                baglanti.Open();
+
+                using (SqlCommand ekle = new SqlCommand())
+                {
+                    ekle.Connection = baglanti;
+                    ekle.CommandType = CommandType.Text;
+                    ekle.CommandText = "insert into personel(adi,soyadi)" +
+                    "values(@FirstName,@LastName)";
+
+                    ekle.Parameters.AddWithValue("@FirstName", txtad.Text);
+                    ekle.Parameters.AddWithValue("@LastName", txtsoyad.Text);
+
+
+                    if (ekle.ExecuteNonQuery() == 1)
+                        MessageBox.Show("kayıt eklendi");
+                    else MessageBox.Show("kayıt eklenemedi");
+
+                }
+                baglanti.Close();
+
+            }
+
+            Form1_Load(this, null);
+        }
+
+        private void btnguncelle_Click(object sender, EventArgs e)
+        {
+            if (txtid.Text == "")
+            {
+                MessageBox.Show("lütfen aranacak veriyi giriniz");
+                return;
+            }
+
+
+            using (SqlConnection baglanti = new SqlConnection())
+            {
+
+                baglanti.ConnectionString = yol;
+                baglanti.Open();
+                using (SqlCommand guncelle = new SqlCommand())
+                {
+
+                    guncelle.Connection = baglanti;
+                    guncelle.CommandType = CommandType.Text;
+                    guncelle.CommandText =
+               "update personel set FirstName=@FirstName,LastName=@LastName where personelID=@ID";
+
+
+                    guncelle.Parameters.AddWithValue("@FirstName", txtad.Text);
+                    guncelle.Parameters.AddWithValue("@LastName", txtsoyad.Text);
+                    guncelle.Parameters.AddWithValue("@ID", txtid.Text);
+
+                    if (guncelle.ExecuteNonQuery() == 1)
+                    {
+                        MessageBox.Show("kayıt guncellendi");
+                    }
+                    else
+                    {
+                        MessageBox.Show("kayıt güncellenemedi");
+                    }
+
+                }
+                baglanti.Close();
+
+            }
+            Form1_Load(this, null);
+        }
+    
+    void temizle()
+    {
+        txtad.Text = "";
+        txtsoyad.Text = "";
+        txtid.Text = "";
+
+    }
+
+
+    private void btnlist_Click(object sender, EventArgs e)
+        {
+            Form1_Load(this, null);
+            temizle();
+        }
+  
+        void Sil(int numara)
+        {
+            using (SqlConnection baglanti = new SqlConnection())
+            {
+
+                baglanti.ConnectionString = yol;
+                baglanti.Open();
+               string sql = "DELETE FROM personel WHERE personelID=@numara";
+                using (SqlCommand komut = new SqlCommand(sql,baglanti))
+                {
+               
+                    komut.Parameters.AddWithValue("@numara", numara);
+                    if (komut.ExecuteNonQuery() == 1)
+                    MessageBox.Show("kayıt silindi");
+                    else
+                        MessageBox.Show("kayıt silindi");
+                }
+                baglanti.Close();
+            }
+
+            
+
+        }
+    
+        private void silToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow drow in dataGridView1.SelectedRows)  //Seçili Satırları Silme
+            {
+                int numara = Convert.ToInt32(drow.Cells[0].Value);
+                Sil(numara);
+                Form1_Load(this, null);
+            }
+        }
+    }
+}
